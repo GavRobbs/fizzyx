635879cf4ebffc158c0eac1995d2c77a655a15c9
Gavin <gnation15@gmail.com>
2022-02-01 13:57:41 -0500

    Added ball speed and restitution to the list of parameters that can be changed in the GUI of the bounce demo.

--- apps/Bounce/bouncedemo.cpp
+++ apps/Bounce/bouncedemo.cpp
@@ -32,18 +32,19 @@ void BounceDemo::recreateDemo()
 {
     fizzyx::PhysicsWorld & pWorld = sceneManager.getPhysicsWorld();
 
-    fizzyx::core::PhysicsMaterial material{0.5f, 0.0f, 0.0f};
+    fizzyx::core::PhysicsMaterial material1{ball1_current_restitution, 0.0f, 0.0f};
+    fizzyx::core::PhysicsMaterial material2{ball2_current_restitution, 0.0f, 0.0f};
 
     ball1 = new Entity{};
     Transform t = ball1->getTransform();
-    t.position = math::Vector2{224.0f, 400.0f};
+    t.position = math::Vector2{324.0f, 400.0f};
     ball1->setTransform(t);
     auto ball1body = new fizzyx::tutorial::PointMassEntity{};
-    ball1body->setVelocity(math::Vector2{16.0f, 0.0f});
+    ball1body->setVelocity(math::Vector2{ball1_current_speed, 0.0f});
     ball1body->setMass(ball1_current_mass);
     ball1body->setGravity(0.0f);
     ball1body->setPosition(t.position);
-    ball1body->setMaterial(material);
+    ball1body->setMaterial(material1);
     pWorld.addEntity(ball1body);
     auto * sdrb1 = new SimpleDynamicRigidbodyComponent{};
     sdrb1->setEntity(ball1body);
@@ -53,14 +54,14 @@ void BounceDemo::recreateDemo()
 
     ball2 = new Entity{};
     Transform t2 = ball2->getTransform();
-    t2.position = math::Vector2{800.0f, 400.0f};
+    t2.position = math::Vector2{700.0f, 400.0f};
     ball2->setTransform(t2);
     auto ball2body = new fizzyx::tutorial::PointMassEntity{};
-    ball2body->setVelocity(math::Vector2{-16.0f, 0.0f});
+    ball2body->setVelocity(math::Vector2{ball2_current_speed, 0.0f});
     ball2body->setMass(ball2_current_mass);
     ball2body->setGravity(0.0f);
     ball2body->setPosition(t2.position);
-    ball2body->setMaterial(material);
+    ball2body->setMaterial(material2);
     pWorld.addEntity(ball2body);
     auto * sdrb2 = new SimpleDynamicRigidbodyComponent{};
     sdrb2->setEntity(ball2body);
@@ -77,11 +78,41 @@ void BounceDemo::reset()
 
     ball1_current_restitution = ball1_default_restitution;
     ball2_current_restitution = ball2_default_restitution;
+
+    ball1_current_speed = ball1_default_speed;
+    ball2_current_speed = ball2_default_speed;
 }
 
 void BounceDemo::drawGUI()
 {
+    ImGui::Begin("Simple Collision", NULL, ImGuiWindowFlags_AlwaysAutoResize);
 
+    ImGui::InputFloat("Red Ball Mass", &ball1_current_mass, 0.5f, 1.0f, "%.1f");
+    ImGui::InputFloat("Red Ball Restitution", &ball1_current_restitution, 0.01f, 0.1f, "%.2f");
+    ImGui::InputFloat("Red Ball Speed", &ball1_current_speed, 0.1f, 1.0f, "%.2f");
+
+    ImGui::InputFloat("Blue Ball Mass", &ball2_current_mass, 0.5f, 1.0f, "%.1f");
+    ImGui::InputFloat("Blue Ball Restitution", &ball2_current_restitution, 0.01f, 0.1f, "%.2f");
+    ImGui::InputFloat("Blue Ball Speed", &ball2_current_speed, 0.1f, 1.0f, "%.2f");
+
+
+    if(ImGui::Button("Restart"))
+    {
+        tearDown();
+        sceneManager.resetScene();
+        recreateDemo();
+    }
+
+    if(ImGui::Button("Reset"))
+    {
+        reset();
+        tearDown();
+        sceneManager.resetScene();
+        recreateDemo();
+    }
+
+    
+    ImGui::End();
 }
 
 void BounceDemo::tearDown()
--- apps/Bounce/bouncedemo.h
+++ apps/Bounce/bouncedemo.h
@@ -22,11 +22,17 @@ class BounceDemo : public DemoApp
     float ball1_current_restitution{ball1_default_restitution};
     float ball2_current_restitution{ball2_default_restitution};
 
-    float ball1_default_mass;
-    float ball2_default_mass;
+    float ball1_default_mass{3.0f};
+    float ball2_default_mass{3.0f};
 
     float ball1_current_mass{ball1_default_mass};
     float ball2_current_mass{ball2_default_mass};
+
+    float ball1_default_speed{15.0f};
+    float ball2_default_speed{-15.0f};
+
+    float ball1_current_speed{ball1_default_speed};
+    float ball2_current_speed{ball2_default_speed};
 
     Entity * ball1;
     Entity * ball2;
