15cc53c274ac10feb4d684de2129559e0c90ebd1
Gavin <gnation15@gmail.com>
2022-02-01 12:57:03 -0500

    Added the circle renderer component.

--- apps/DemoCore/basiccomponents.cpp
+++ apps/DemoCore/basiccomponents.cpp
@@ -1,6 +1,6 @@
 #include <basiccomponents.h>
 
-BoxRendererComponent::BoxRendererComponent(const GraphicsManager& _graphics, const GraphicsManager::Color& color, const math::Vector2& dimensions):Component(), graphicsManager(_graphics), color(color), dimensions(dimensions)
+BoxRendererComponent::BoxRendererComponent(const GraphicsManager& _graphics, const GraphicsManager::Color& color, const math::Vector2& dimensions, bool filled):Component(), graphicsManager(_graphics), color(color), dimensions(dimensions), filled(filled)
 {
 
 }
@@ -17,7 +17,39 @@ const math::Vector2 & BoxRendererComponent::getDimensions()
 
 void BoxRendererComponent::render(float dt)
 {
-    graphicsManager.drawRectFilled(this->getOwner()->getTransform().position, dimensions.x, dimensions.y, color);
+    if(filled)
+    {
+        graphicsManager.drawRectFilled(this->getOwner()->getTransform().position, dimensions.x, dimensions.y, color);
+    } else
+    {
+        graphicsManager.drawRectOutline(this->getOwner()->getTransform().position, dimensions.x, dimensions.y, color);
+    }
+}
+
+CircleRendererComponent::CircleRendererComponent(const GraphicsManager& _graphics, const GraphicsManager::Color& color, const float & radius, bool filled=true):Component(), graphicsManager(_graphics), color(color), radius(radius), filled(filled)
+{
+
+}
+
+void CircleRendererComponent::setRadius(const float & radius)
+{
+    this->radius = radius;
+}
+
+const float & CircleRendererComponent::getRadius()
+{
+    return radius;
+}
+
+void CircleRendererComponent::render(float dt)
+{
+    if(filled)
+    {
+        graphicsManager.drawCircleFilled(this->getOwner()->getTransform().position, radius, color);
+    } else
+    {
+        graphicsManager.drawCircleOutline(this->getOwner()->getTransform().position, radius, color);
+    }
 }
 
 
--- apps/DemoCore/basiccomponents.h
+++ apps/DemoCore/basiccomponents.h
@@ -5,12 +5,12 @@
 #include <graphicsmanager.h>
 #include <core/physicsentity.h>
 
-/* A class to draw the box on the end of our spring*/
+/* Draws a simple colored box, can be filled or an outline */
 class BoxRendererComponent : public IRenderable, public Component
 {
     public:
     void render(float dt=0.0f) override;
-    BoxRendererComponent(const GraphicsManager& _graphics, const GraphicsManager::Color& color, const math::Vector2& dimensions);
+    BoxRendererComponent(const GraphicsManager& _graphics, const GraphicsManager::Color& color, const math::Vector2& dimensions, bool filled=true);
 
     void setDimensions(const math::Vector2 & dimensions);
     const math::Vector2 & getDimensions();
@@ -19,8 +19,27 @@ class BoxRendererComponent : public IRenderable, public Component
     const GraphicsManager &graphicsManager;
     math::Vector2 dimensions{};
     GraphicsManager::Color color;
+    bool filled;
 };
 
+/* Draws a simple colored circle, can be filled or an outline */
+class CircleRendererComponent : public IRenderable, public Component
+{
+    public:
+    void render(float dt=0.0f) override;
+    CircleRendererComponent(const GraphicsManager& _graphics, const GraphicsManager::Color& color, const float & radius, bool filled=true);
+
+    void setRadius(const float & radius);
+    const float & getRadius();
+
+    private:
+    const GraphicsManager &graphicsManager;
+    float radius;
+    GraphicsManager::Color color;
+    bool filled;
+};
+
+/* Puts a body in the scene under the control of the physics engine. Only handles linear kinematic properties.*/
 class SimpleDynamicRigidbodyComponent : public IPhysicsable, public Component
 {
     public:
