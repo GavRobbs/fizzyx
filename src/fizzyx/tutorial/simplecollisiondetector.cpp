6903be52083dcb9955c32647593fc5ffb9d53d6f
Gavin <gnation15@gmail.com>
2022-02-05 13:38:18 -0500

    Created the collision detector loop for SimpleCollisionDetector.

--- src/fizzyx/tutorial/simplecollisiondetector.cpp
+++ src/fizzyx/tutorial/simplecollisiondetector.cpp
@@ -4,6 +4,31 @@ using namespace fizzyx::tutorial;
 
 fizzyx::collision::ICollisionData * SimpleCollisionDetector::detectCollision(fizzyx::core::IPhysicsEntity * a, fizzyx::core::IPhysicsEntity * b)
 {
+    for(auto it = entries.begin(); it != entries.end(); ++it)
+    {
+        int compat = it->isCompatible(a->getBodyShape()->getShapeTypeId(),
+                        b->getBodyShape()->getShapeTypeId());
+
+        switch (compat)
+        {
+            case 0:
+            {
+                break;
+            }
+            case 1:
+            {
+                return it->evaluator(a->getBodyShape(), a->getPosition(), a->getRotation(), b->getBodyShape(), b->getPosition(), b->getRotation());
+            }
+            case -1:
+            {
+                fizzyx::collision::ICollisionData * result = it->evaluator(a->getBodyShape(), a->getPosition(), a->getRotation(), b->getBodyShape(), b->getPosition(), b->getRotation());
+                result->invert();
+                return result;
+            }   
+            default:
+                break;
+        }
+    }
     return nullptr;
 }
 
